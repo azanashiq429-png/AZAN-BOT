3f507622529448409cee42470331af38d2990e17
azanashiq429-png <azanashiq429@gmail.com>
2026-07-17 16:51:52 +0500

    Create video.js

---
+++ modules/video.js
@@ -0,0 +1,11 @@
+// modules/video.js
+module.exports = {
+    getTutorialDetails: () => {
+        return {
+            title: "AZAN VIP Exploit & Port Bypassing Guide",
+            duration: "04 Mins : 18 Secs",
+            videoUrl: "https://www.youtube.com/embed/your-video-id", // Apni video link yahan lagayein
+            status: "active"
+        };
+    }
+};
